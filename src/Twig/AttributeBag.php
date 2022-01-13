35e5d0716923b3868c25104fece0270eedbd9eef
Kevin Bond <kevinbond@gmail.com>
2022-01-13 18:57:31 -0500

    add additional AttributeBag helper methods

--- src/Twig/AttributeBag.php
+++ src/Twig/AttributeBag.php
@@ -27,4 +27,46 @@ final class AttributeBag
             ''
         );
     }
+
+    public function prepend(array $with): self
+    {
+        foreach ($this->attributes as $key => $value) {
+            $with[$key] = isset($with[$key]) ? "{$with[$key]} {$value}" : $value;
+        }
+
+        return new self($with);
+    }
+
+    public function append(array $with): self
+    {
+        foreach ($this->attributes as $key => $value) {
+            $with[$key] = isset($with[$key]) ? "{$value} {$with[$key]}" : $value;
+        }
+
+        return new self($with);
+    }
+
+    public function only(string ...$keys): self
+    {
+        $attributes = [];
+
+        foreach ($this->attributes as $key => $value) {
+            if (in_array($key, $keys, true)) {
+                $attributes[$key] = $value;
+            }
+        }
+
+        return new self($attributes);
+    }
+
+    public function reject(string ...$keys): self
+    {
+        $clone = clone $this;
+
+        foreach ($keys as $key) {
+            unset($clone->attributes[$key]);
+        }
+
+        return $clone;
+    }
 }
