351468da9fe7720c364e9eed6143b436f74b29d4
Kevin Bond <kevinbond@gmail.com>
2022-01-13 17:58:00 -0500

    add ux-twig-component and sample component

---
+++ src/Twig/WithAttributesComponent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Twig;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+/**
+ * @author Kevin Bond <kevinbond@gmail.com>
+ */
+#[AsTwigComponent('with_attributes')]
+final class WithAttributesComponent
+{
+
+}
