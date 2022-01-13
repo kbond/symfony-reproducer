7c16217a76581b34c2c5d07ba23bf17ec2d48d2f
Kevin Bond <kevinbond@gmail.com>
2022-01-13 18:21:13 -0500

    demonstrate removing the need for raw

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,22 @@
 
 namespace App\Controller;
 
+use App\Twig\AttributeBag;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Extension\EscaperExtension;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'default')]
     public function index(): Response
     {
+        $this->container->get('twig')
+            ->getExtension(EscaperExtension::class)
+            ->addSafeClass(AttributeBag::class, ['html'])
+        ;
+
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
