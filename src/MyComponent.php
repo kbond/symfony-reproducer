7cf55eac7378248145a7ab5d91d617963fd37ca3
Kevin Bond <kevinbond@gmail.com>
2022-01-14 13:35:26 -0500

    add controller and component

---
+++ src/MyComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+/**
+ * @author Kevin Bond <kevinbond@gmail.com>
+ */
+#[AsTwigComponent('my_component')]
+final class MyComponent
+{
+}
