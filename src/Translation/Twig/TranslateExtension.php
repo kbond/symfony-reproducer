0bdb42a73577f87c9063191d7fe31d95d61a67b5
Kevin Bond <kevinbond@gmail.com>
2022-03-19 07:41:40 -0400

    refactor

--- src/Translation/Twig/TranslateExtension.php
+++ src/Translation/Twig/TranslateExtension.php
@@ -4,21 +4,40 @@ namespace App\Translation\Twig;
 
 use App\Translation\TranslatableProxy;
 use App\Translation\TranslationManager;
+use Psr\Container\ContainerInterface;
+use Symfony\Contracts\Service\ServiceSubscriberInterface;
+use Symfony\Contracts\Translation\LocaleAwareInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
 /**
  * @author Kevin Bond <kevinbond@gmail.com>
  */
-final class TranslateExtension extends AbstractExtension
+final class TranslateExtension extends AbstractExtension implements ServiceSubscriberInterface, LocaleAwareInterface
 {
-    public function __construct(private string $defaultLocale, private TranslationManager $manager)
+    private ?string $currentLocale = null;
+
+    public function __construct(private string $defaultLocale, private ContainerInterface $container)
     {
+    }
+
+    public static function getSubscribedServices(): array
+    {
+        return [TranslationManager::class];
+    }
+
+    public function setLocale(string $locale): void
+    {
+        $this->currentLocale = $locale;
+    }
+
+    public function getLocale(): string
+    {
+        return $this->currentLocale ?? $this->defaultLocale;
     }
 
     public function getFilters(): array
     {
-        // TODO: twig runtime
         return [
             new TwigFilter('translate', [$this, 'translate'])
         ];
@@ -33,12 +52,12 @@ final class TranslateExtension extends AbstractExtension
      */
     public function translate(object $object, ?string $locale = null): object
     {
-        $locale = $locale ?? \Locale::getDefault();
+        $locale = $locale ?? $this->getLocale();
 
         if ($locale === $this->defaultLocale) {
             return $object;
         }
 
-        return $this->manager->proxyFor($object, $locale);
+        return $this->container->get(TranslationManager::class)->proxyFor($object, $locale);
     }
 }
