93e96360ae8f1c92cbd9ccfa1325a0ba2b610536
Kevin Bond <kevinbond@gmail.com>
2022-04-19 15:30:27 -0400

    add initial csrf support

--- lib/src/FormRequest.php
+++ lib/src/FormRequest.php
@@ -5,6 +5,8 @@ namespace Zenstruck;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Csrf\CsrfToken;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintViolationInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
@@ -19,6 +21,14 @@ use Zenstruck\FormRequest\FormState\InMemoryFormState;
  */
 class FormRequest implements ServiceSubscriberInterface
 {
+    // todo make these configurable
+    private const DEFAULT_CSRF_TOKEN_ID = 'form';
+    private const CSRF_TOKEN_FIELD = '_token';
+    private const CSRF_TOKEN_HEADER = 'X-CSRF-TOKEN';
+
+    // todo make globally configurable
+    private ?string $csrfTokenId = self::DEFAULT_CSRF_TOKEN_ID;
+
     private ContainerInterface $container;
 
     final public function __call(string $name, array $arguments): mixed
@@ -43,6 +53,7 @@ class FormRequest implements ServiceSubscriberInterface
         return [
             RequestStack::class,
             ValidatorInterface::class,
+            '?'.CsrfTokenManagerInterface::class,
         ];
     }
 
@@ -75,16 +86,46 @@ class FormRequest implements ServiceSubscriberInterface
             }
         }
 
-        // TODO: check csrf
-        // TODO: configure globally in config (enabled/disabled, default token_id, default token_field)
-        // TODO: Allow CSRF Request Header
+        if (!$this->isCsrfEnabled()) {
+            return $state;
+        }
+
+        $token = $this->request->get(self::CSRF_TOKEN_FIELD, $this->headers->get(self::CSRF_TOKEN_HEADER));
+
+        if (!$this->isCsrfTokenValid($this->csrfTokenId, $token)) {
+            // TODO: alternate behaviour: throw TokenMismatch exception to convert to 419 in event listener
+            $state->addGlobalError('The CSRF token is invalid. Please try to resubmit the form.');
+        }
 
         return $state;
+    }
+
+    final public function enableCsrf(string $tokenId = self::DEFAULT_CSRF_TOKEN_ID): self
+    {
+        $this->csrfTokenId = $tokenId;
+
+        return $this;
+    }
+
+    final public function disableCsrf(): self
+    {
+        $this->csrfTokenId = null;
+
+        return $this;
     }
 
     final public function isSubmitted(): bool
     {
         return !$this->isMethodCacheable();
+    }
+
+    final public function isCsrfTokenValid(string $id, ?string $token): bool
+    {
+        if (!$this->container->has(CsrfTokenManagerInterface::class)) {
+            throw new \LogicException('CSRF not enabled in your application.');
+        }
+
+        return $this->container->get(CsrfTokenManagerInterface::class)->isTokenValid(new CsrfToken($id, $token));
     }
 
     /**
@@ -98,5 +139,10 @@ class FormRequest implements ServiceSubscriberInterface
     final public function unwrap(): Request
     {
         return $this->container->get(RequestStack::class)->getCurrentRequest() ?? throw new \LogicException(\sprintf('%s can only be used within the scope of a request.', static::class));
+    }
+
+    private function isCsrfEnabled(): bool
+    {
+        return null !== $this->csrfTokenId;
     }
 }
