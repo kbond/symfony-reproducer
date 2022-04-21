d46db1a8145adbfd548591fac113d891fbaf7d04
Kevin Bond <kevinbond@gmail.com>
2022-04-21 10:56:37 -0400

    Json extends Serialized

--- src/View/Json.php
+++ src/View/Json.php
@@ -2,48 +2,19 @@
 
 namespace App\View;
 
-use App\View;
-use Psr\Container\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * @author Kevin Bond <kevinbond@gmail.com>
  */
-final class Json extends View
+final class Json extends Serialized
 {
     /**
      * @param array<string,mixed> $context
      */
-    protected function __construct(private mixed $data, private array $context = [])
+    protected function __construct(mixed $data, array $context = [])
     {
-        parent::__construct();
-    }
-
-    /**
-     * @internal
-     */
-    public function __invoke(Request $request, ContainerInterface $container, ?Response $response = null): Response
-    {
-        $serializer = $container->has(SerializerInterface::class) ? $container->get(SerializerInterface::class) : null;
-
-        if ($this->context && !$serializer) {
-            throw new \LogicException('Cannot use serializer context without serializer. Try running "composer require serializer".');
-        }
-
-        if (!$serializer) {
-            return parent::__invoke($request, $container, new JsonResponse($this->data));
-        }
-
-        return parent::__invoke($request, $container, new JsonResponse(
-            $serializer->serialize($this->data, 'json', [
-                'json_encode_options' => JsonResponse::DEFAULT_ENCODING_OPTIONS,
-                ...$this->context
-            ]),
-            json: true,
-        ));
+        parent::__construct($data, $context, 'json');
     }
 
     /**
--- src/View/Serialized.php
+++ src/View/Serialized.php
@@ -4,6 +4,7 @@ namespace App\View;
 
 use App\View;
 use Psr\Container\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -11,11 +12,9 @@ use Symfony\Component\Serializer\SerializerInterface;
 /**
  * @author Kevin Bond <kevinbond@gmail.com>
  */
-final class Serialized extends View
+class Serialized extends View
 {
-    private ?string $format = null;
-
-    protected function __construct(private mixed $data, private array $context = [])
+    protected function __construct(private mixed $data, private array $context = [], private ?string $format = null)
     {
         parent::__construct();
     }
@@ -23,36 +22,41 @@ final class Serialized extends View
     /**
      * @internal
      */
-    public function __invoke(Request $request, ContainerInterface $container, ?Response $response = null): Response
+    final public function __invoke(Request $request, ContainerInterface $container, ?Response $response = null): Response
     {
-        if (!$container->has(SerializerInterface::class)) {
+        // get format in order: 1. manually set, _format request attribute, request accept header
+        $format = $this->format ?? $request->getRequestFormat(null) ?? $request->getPreferredFormat();
+        $serializer = $container->has(SerializerInterface::class) ? $container->get(SerializerInterface::class) : null;
+        $context = $this->context;
+
+        if ('json' === $format && !$serializer) {
+            if ($context) {
+                throw new \LogicException('Cannot use serializer context without serializer. Try running "composer require serializer".');
+            }
+
+            return parent::__invoke($request, $container, new JsonResponse($this->data));
+        }
+
+        if (!$serializer) {
             throw new \LogicException(\sprintf('The serializer is required to use "%s". Try running "composer require serializer".', self::class));
         }
 
-        // get format in order: 1. manually set, _format request attribute, request accept header
-        $format = $this->format ?? $request->getRequestFormat(null) ?? $request->getPreferredFormat();
+        if ('json' === $format) {
+            $context['json_encode_options'] = JsonResponse::DEFAULT_ENCODING_OPTIONS;
+        }
+
+        $serialized = $serializer->serialize($this->data, $format, $context);
         $mimeType = $request->getMimeType($format);
+        $headers = $mimeType ? ['Content-Type' => $request->getMimeType($format)] : [];
 
-        return parent::__invoke($request, $container, new Response(
-            $container->get(SerializerInterface::class)->serialize($this->data, $format, $this->context),
-            headers: $mimeType ? ['Content-Type' => $request->getMimeType($format)] : [],
-        ));
-    }
+        if ('json' === $format) {
+            return parent::__invoke($request, $container, new JsonResponse(
+                $serialized,
+                headers: $headers,
+                json: true,
+            ));
+        }
 
-    public function as(string $format): self
-    {
-        $this->format = $format;
-
-        return $this;
-    }
-
-    public function asJson(): self
-    {
-        return $this->as('json');
-    }
-
-    public function asXml(): self
-    {
-        return $this->as('xml');
+        return parent::__invoke($request, $container, new Response($serialized, headers: $headers));
     }
 }
