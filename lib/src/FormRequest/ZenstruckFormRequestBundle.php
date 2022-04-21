03d4b4483d31a034cc63b2b793a7beadf294beab
Kevin Bond <kevinbond@gmail.com>
2022-04-21 16:11:43 -0400

    remove session factory/session form state

--- lib/src/FormRequest/ZenstruckFormRequestBundle.php
+++ lib/src/FormRequest/ZenstruckFormRequestBundle.php
@@ -2,22 +2,11 @@
 
 namespace Zenstruck\FormRequest;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Zenstruck\FormRequest\DependencyInjection\FormRequestPass;
 
 /**
  * @author Kevin Bond <kevinbond@gmail.com>
  */
 final class ZenstruckFormRequestBundle extends Bundle
 {
-    /**
-     * @internal
-     */
-    public function build(ContainerBuilder $container): void
-    {
-        parent::build($container);
-
-        $container->addCompilerPass(new FormRequestPass());
-    }
 }
