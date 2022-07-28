6c615c788c70568981f8f012aca6995a2f142773
Kevin Bond <kevinbond@gmail.com>
2022-07-28 14:58:06 -0400

    complete current tests

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -17,6 +17,6 @@ class Post
     #[ORM\Column(length: 255)]
     public string $title;
 
-    #[ORM\Column(type: 'file', options: ['filesystem' => 'public'])]
-    public File $file;
+    #[ORM\Column(type: 'file', options: ['filesystem' => 'public'], nullable: true)]
+    public ?File $file = null;
 }
--- tests/FilesystemTest.php
+++ tests/FilesystemTest.php
@@ -3,7 +3,9 @@
 namespace App\Tests;
 
 use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Zenstruck\Filesystem\Node\File\PendingFile;
 use Zenstruck\Filesystem\Test\InteractsWithFilesystem;
 use Zenstruck\Filesystem\Test\Node\TestFile;
 use Zenstruck\Foundry\Test\Factories;
@@ -33,13 +35,131 @@ class FilesystemTest extends KernelTestCase
     /**
      * @test
      */
-    public function entities(): void
+    public function entity_save_load_delete(): void
     {
+        $this->filesystem()->assertNotExists('nested/file.txt');
+
         $post = create(Post::class, [
             'title' => 'foobar',
             'file' => $this->filesystem()->write('nested/file.txt', 'content')->last()
         ]);
 
-        dd(repository(Post::class)->first()->object()->file->mimeType());
+        $this->assertSame('text/plain', repository(Post::class)->first()->object()->file->mimeType());
+        $this->filesystem()->assertExists('nested/file.txt');
+
+        $post->remove();
+
+        $this->filesystem()->assertNotExists('nested/file.txt');
+        $post->assertNotPersisted();
+    }
+
+    /**
+     * @test
+     */
+    public function entity_delete_file_on_update(): void
+    {
+        $this->filesystem()->assertNotExists('nested/file.txt');
+
+        $post = create(Post::class, [
+            'title' => 'foobar',
+            'file' => $this->filesystem()->write('nested/file.txt', 'content')->last()
+        ]);
+
+        $this->assertSame('text/plain', repository(Post::class)->first()->object()->file->mimeType());
+        $this->filesystem()->assertExists('nested/file.txt');
+
+        $post->file = null;
+        $post->save();
+
+        $this->filesystem()->assertNotExists('nested/file.txt');
+        $post->assertPersisted();
+    }
+
+    /**
+     * @test
+     */
+    public function can_update_file_on_null_file(): void
+    {
+        $this->filesystem()->assertNotExists('nested/file.txt');
+
+        $post = create(Post::class, [
+            'title' => 'foobar',
+            'file' => null,
+        ]);
+
+        $post->file = $this->filesystem()->write('nested/file.txt', 'content')->last();
+        $post->save();
+
+        $this->assertSame('text/plain', repository(Post::class)->first()->object()->file->mimeType());
+        $this->filesystem()->assertExists('nested/file.txt');
+    }
+
+    /**
+     * @test
+     */
+    public function can_update_file_on_existing_file(): void
+    {
+        $post = create(Post::class, [
+            'title' => 'foobar',
+            'file' => $this->filesystem()->write('nested/file1.txt', 'content')->last(),
+        ]);
+
+        $post->file = $this->filesystem()->write('nested/file2.png', 'content')->last();
+        $post->save();
+
+        $this->assertSame('image/png', repository(Post::class)->first()->object()->file->mimeType());
+        $this->filesystem()->assertNotExists('nested/file1.txt');
+        $this->filesystem()->assertExists('nested/file2.png');
+    }
+
+    /**
+     * @test
+     */
+    public function can_create_with_pending_file(): void
+    {
+        $this->filesystem()->assertNotExists('composer.json');
+
+        $post = create(Post::class, [
+            'title' => 'foobar',
+            'file' => new PendingFile(__DIR__.'/../composer.json'),
+        ]);
+
+        $this->filesystem()->assertExists('composer.json');
+        $this->assertSame('application/json', repository(Post::class)->first()->file->mimeType());
+    }
+
+    /**
+     * @test
+     */
+    public function can_update_pending_file_on_null_file(): void
+    {
+        $post = create(Post::class, [
+            'title' => 'foobar',
+            'file' => null,
+        ]);
+
+        $post->file = new PendingFile(__DIR__.'/../composer.json');
+        $post->save();
+
+        $this->assertSame('application/json', repository(Post::class)->first()->object()->file->mimeType());
+        $this->filesystem()->assertExists('composer.json');
+    }
+
+    /**
+     * @test
+     */
+    public function can_update_pending_file_on_existing_file(): void
+    {
+        $post = create(Post::class, [
+            'title' => 'foobar',
+            'file' => $this->filesystem()->write('nested/file1.txt', 'content')->last(),
+        ]);
+
+        $post->file = new PendingFile(__DIR__.'/../composer.json');
+        $post->save();
+
+        $this->assertSame('application/json', repository(Post::class)->first()->object()->file->mimeType());
+        $this->filesystem()->assertNotExists('nested/file1.txt');
+        $this->filesystem()->assertExists('composer.json');
     }
 }
