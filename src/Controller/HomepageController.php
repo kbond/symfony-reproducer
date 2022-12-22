b6acdb4e8daec8afda2c6495b24b9f95f8d8f84b
Kevin Bond <kevinbond@gmail.com>
2022-12-22 17:18:48 -0500

    add stuff to controller

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,12 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Zenstruck\Filesystem;
 
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    public function index(Filesystem $filesystem, Filesystem $privateFilesystem): Response
     {
+        $filesystem->write('foo.txt', 'content')
+            ->delete('foo.txt')
+        ;
+
+        $privateFilesystem->write('foo.txt', 'content')
+            ->delete('foo.txt')
+            ->write('foo.txt', 'content')
+            ->delete('foo.txt')
+        ;
+
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
         ]);
