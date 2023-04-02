fbca458379df462930a620132379bb17c76bcceb
Kevin Bond <kevinbond@gmail.com>
2023-04-02 09:44:20 -0400

    add encore

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,21 +2,15 @@
 
 namespace App\Controller;
 
-use App\Message\MessageA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(MessageBusInterface $bus): Response
+    public function index(): Response
     {
-        foreach (\range(1, 10) as $i) {
-            $bus->dispatch(new MessageA('my message!', (bool) \random_int(0, 100) < 10));
-        }
-
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
         ]);
