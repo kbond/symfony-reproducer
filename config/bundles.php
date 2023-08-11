2a5746148fa6bfb0be2bc432f024e54652ec099d
Kevin Bond <kevinbond@gmail.com>
2023-08-11 14:17:57 -0400

    add live components

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
 ];
