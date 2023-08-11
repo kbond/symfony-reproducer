01d224e679566fb3792853b1d9cb3c0bd9b4b2e5
Kevin Bond <kevinbond@gmail.com>
2023-08-11 16:17:30 -0400

    send emails

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,19 +5,32 @@ namespace App\Controller;
 use App\Message\MessageA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Header\TagHeader;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Zenstruck\Messenger\Monitor\Stamp\Tag;
 
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(MessageBusInterface $bus): Response
+    public function index(MessageBusInterface $bus, MailerInterface $mailer): Response
     {
-        $bus->dispatch(new MessageA('from controller 1'));
-        $bus->dispatch(new MessageA('from controller 2', throw: true));
-        $bus->dispatch(new MessageA('from controller 3'));
-        $bus->dispatch(new MessageA('from controller 4'), [new Tag('forth')]);
+//        $bus->dispatch(new MessageA('from controller 1'));
+//        $bus->dispatch(new MessageA('from controller 2', throw: true));
+//        $bus->dispatch(new MessageA('from controller 3'));
+//        $bus->dispatch(new MessageA('from controller 4'), [new Tag('forth')]);
+
+        $email = (new Email())
+            ->subject('some subject')
+            ->from('admin@example.com')
+            ->to('kevin@example.com')
+            ->text('some body')
+        ;
+        $email->getHeaders()->add(new TagHeader('example'));
+
+        $mailer->send($email);
 
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
