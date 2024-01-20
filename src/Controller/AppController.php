f62cbaa0dac413f8071475aaef8c03465964b635
Kevin Bond <kevinbond@gmail.com>
2024-01-20 11:18:31 -0500

    cleanup

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
-use App\FormRequest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Validator\Constraints\Email;
@@ -18,22 +23,30 @@ class AppController extends AbstractController
     }
 
     #[Route('/form1', name: 'form1')]
-    public function form1(FormRequest $request): Response
+    public function form1(Request $request): Response
     {
-        $form = $request->validate([
-            'name' => NotBlank::class,
-            'email' => [NotBlank::class, Email::class],
-            'bio' => null,
-            'terms' => null,
-        ]);
+        $form = $this->createFormBuilder()
+            ->add('name', TextType::class, [
+                'constraints' => new NotBlank(),
+            ])
+            ->add('email', EmailType::class, [
+                'constraints' => [new NotBlank(), new Email()],
+            ])
+            ->add('bio', TextareaType::class, [
+                'required' => false,
+            ])
+            ->add('terms', CheckboxType::class)
+            ->add('submit', SubmitType::class)
+            ->getForm()
+            ->handleRequest($request)
+        ;
 
-        if ($form->isSubmittedAndValid()) {
-            dd($form->data());
+        if ($form->isSubmitted() && $form->isValid()) {
+            dd($form->getData());
         }
 
-        return new Response(
-            $this->renderView('form1.html.twig', ['form' => $form]),
-            $form->isValid() ? 200 : 422
-        );
+        return $this->render('form1.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
