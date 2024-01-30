aa96459acbc320e305902cafcd5377e78d1bfedc
Kevin Bond <kevinbond@gmail.com>
2024-01-30 16:45:28 -0500

    remove svg attributes entirely

--- src/Icon.php
+++ src/Icon.php
@@ -9,17 +9,12 @@ use Symfony\Component\DomCrawler\Crawler;
  */
 final class Icon
 {
-    public readonly array $attributes;
     public readonly string $content;
 
     public function __construct(string $svg)
     {
         $crawler = (new Crawler($svg))->filter('svg');
-        $attr = array_map(static fn(\DomAttr $attr) => $attr->value, iterator_to_array($crawler->getNode(0)->attributes));
 
-        unset($attr['class'], $attr['width'], $attr['height']);
-
-        $this->attributes = $attr;
         $this->content = $crawler->html();
     }
 }
