ddcd85c9bdffb5fd65994cc67d6a933fa4318777
Kevin Bond <kevinbond@gmail.com>
2024-01-31 13:19:32 -0500

    use ux-icons package

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
 ];
