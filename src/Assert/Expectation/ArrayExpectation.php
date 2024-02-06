081e6f85b1b3d7a33fbbea78cdc3af480a051a96
Kevin Bond <kevinbond@gmail.com>
2024-02-06 14:22:19 -0500

    refactor ArrayExpectation

--- src/Assert/Expectation/ArrayExpectation.php
+++ src/Assert/Expectation/ArrayExpectation.php
@@ -20,6 +20,26 @@ final class ArrayExpectation extends Expectation
     use TraversableExpectations;
 
     /**
+     * @param string $selector JMESPath selector
+     */
+    public function search(string $selector): PrimaryExpectation
+    {
+        if (!\function_exists('JmesPath\search')) {
+            throw new \LogicException('"mtdowling/jmespath.php" is required (composer require --dev mtdowling/jmespath.php).');
+        }
+
+        return $this->transform(new PrimaryExpectation(search($selector, $this->value)));
+    }
+
+    public function get(int|string $key): PrimaryExpectation
+    {
+        return $this
+            ->toHave($key)
+            ->transform(new PrimaryExpectation($this->value[$key]))
+        ;
+    }
+
+    /**
      * @param string  $message Available context: {value}
      * @param Context $context
      */
@@ -69,56 +89,12 @@ final class ArrayExpectation extends Expectation
      * @param string  $message Available context: {haystack}, {key}
      * @param Context $context
      */
-    public function toHaveKey(string|int $key, string $message = 'Expected {haystack} to <NOT>have key {key}.', array $context = []): static
+    public function toHave(string|int $key, string $message = 'Expected {haystack} to <NOT>have key {key}.', array $context = []): static
     {
         return $this->ensureTrue(
             \array_key_exists($key, $this->value),
             $message,
             array_merge($context, ['haystack' => $this->value, 'key' => $key])
         );
-    }
-
-    /**
-     * @param string  $selector JMESPath selector
-     * @param string  $message  Available context: {actual}, {expected}, {selector}, {value}
-     * @param Context $context
-     */
-    public function toMatch(string $selector, mixed $expected, string $message = 'Expected {selector} which matches {actual} to <NOT>match {expected} on {value}.', array $context = []): static
-    {
-        $actual = $this->search($selector);
-
-        return $this->ensureTrue(
-            $actual === $expected,
-            $message,
-            array_merge($context, ['actual' => $actual, 'expected' => $expected, 'selector' => $selector, 'value' => $this->value])
-        );
-    }
-
-    /**
-     * @param string  $selector JMESPath selector
-     * @param string  $message  Available context: {haystack}, {selector}
-     * @param Context $context
-     */
-    public function toHave(string $selector, string $message = 'Expected {haystack} to <NOT>have JMESPath {selector}.', array $context = []): static
-    {
-        $prefix = 1 === preg_match('#^\[#', $selector) ? '' : '@.';
-        $exploded = explode('.', $prefix.$selector);
-        $key = array_pop($exploded);
-        $keySelector = implode('.', $exploded);
-
-        return $this->ensureTrue(
-            true === $this->search("contains(keys({$keySelector}), '{$key}')"),
-            $message,
-            array_merge($context, ['haystack' => $this->value, 'selector' => $selector])
-        );
-    }
-
-    private function search(string $jmesPathSelector): mixed
-    {
-        if (!\function_exists('JmesPath\search')) {
-            throw new \LogicException('"mtdowling/jmespath.php" is required (composer require --dev mtdowling/jmespath.php).');
-        }
-
-        return search($jmesPathSelector, $this->value);
     }
 }
