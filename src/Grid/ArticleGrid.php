d9311f77b2a2932853ddebdc51408a43dd68c68c
Kevin Bond <kevinbond@gmail.com>
2024-03-06 19:23:56 -0500

    simplified grid

--- src/Grid/ArticleGrid.php
+++ src/Grid/ArticleGrid.php
@@ -3,7 +3,6 @@
 namespace App\Grid;
 
 use App\Entity\Article;
-use Zenstruck\Collection\Grid\Formatter\DateTimeFormatter;
 use Zenstruck\Collection\Grid\GridBuilder;
 use Zenstruck\Collection\Grid\GridDefinition;
 use Zenstruck\Collection\Symfony\Attributes\ForObject;
@@ -17,18 +16,15 @@ final class ArticleGrid implements GridDefinition
     public function configure(GridBuilder $builder): void
     {
         $builder
-            ->addColumn('id', visible: false, defaultSort: 'asc')
+            ->addColumn('id', defaultSort: 'asc')
             ->addColumn('title', searchable: true, autofilter: true)
             ->addColumn('description', searchable: true, autofilter: true)
-            ->addColumn('body', searchable: true, visible: false)
+            ->addColumn('body', searchable: true)
             ->addColumn('publishedAt',
                 sortable: true,
                 autofilter: true,
-                formatters: DateTimeFormatter::date(),
             )
             ->addColumn('views', sortable: true, autofilter: true)
-            ->addAction('show', 'app_article_show', ['id' => '@id'])
-            ->addAction('edit', 'app_article_edit', ['id' => '@id'])
         ;
     }
 }
