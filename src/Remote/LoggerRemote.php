c5dea1062c024bc723ff09f7233beedf9adfb0e7
Kevin Bond <kevinbond@gmail.com>
2024-07-22 15:51:10 -0400

    Target

--- src/Remote/LoggerRemote.php
+++ src/Remote/LoggerRemote.php
@@ -4,6 +4,7 @@ namespace App\Remote;
 
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
+use Symfony\Component\DependencyInjection\Attribute\Target;
 
 /**
  * @author Kevin Bond <kevinbond@gmail.com>
@@ -13,17 +14,18 @@ final class LoggerRemote implements RemoteInterface
 {
     public function __construct(
         private RemoteInterface $inner,
-        private LoggerInterface $logger,
+        #[Target('buttonsLogger')]
+        private LoggerInterface $buttonLogger,
     ) {
     }
 
     public function press(string $id): void
     {
-        $this->logger->info('Pressing button "{id}"', ['id' => $id]);
+        $this->buttonLogger->info('Pressing button "{id}"', ['id' => $id]);
 
         $this->inner->press($id);
 
-        $this->logger->info('Pressed button "{id}"', ['id' => $id]);
+        $this->buttonLogger->info('Pressed button "{id}"', ['id' => $id]);
     }
 
     public function buttons(): iterable
